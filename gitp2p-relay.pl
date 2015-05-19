ffc8274d046bf746799571b2ab09830c3291391b
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-05-19 13:46:11 +0300

    makes the relay switch between operations

--- gitp2p-relay.pl
+++ gitp2p-relay.pl
@@ -22,21 +22,38 @@ $loop->listen(
         $stream->configure(
             on_read => sub {
                 my ($self, $buffref, $eof) = @_;
+                return 0 if $eof;
 
-                while ($$buffref =~ s/^(.*:.*)//) {
-                    my $addr = $self->read_handle->peerhost;
-                    my $port = $self->read_handle->peerport;
-                    my $peer_entry = $1 . ' ^ ' . $addr . ':' . $port . "\n";
-                    print "Received entry: $peer_entry";
+                my ($op_name, $op_data) = split / /, $$buffref;
+                print "op: '$op_name', data: '$op_data'\n";
 
-                    my $peers = path("peers");
-                    if ($peers->exists && grep { /\Q$peer_entry\E/ } $peers->lines) {
-                        $self->write("NACK: already added\n");
-                    } else {
-                        $self->write("ACK!\n");
-                        $peers->append(($peer_entry));
+                if ($op_name =~ /upload/) {
+                    if ($op_data =~ /^(.*:.*)/) {
+                        my $addr = $self->read_handle->peerhost;
+                        my $port = $self->read_handle->peerport;
+                        my $peer_entry = $1 . ' ^ ' . $addr . ':' . $port . "\n";
+                        print "Received entry: $peer_entry";
+
+                        my $peers = path("peers");
+                        if ($peers->exists && grep { /\Q$peer_entry\E/ } $peers->lines) {
+                            $self->write("NACK: already added\n");
+                        } else {
+                            $self->write("ACK!\n");
+                            $peers->append(($peer_entry));
+                        }
                     }
+                } 
+                elsif ($op_name =~ /push/) {
                 }
+                elsif ($op_name =~ /fetch/) {
+                }
+                elsif ($op_name =~ /list/) {
+                }
+                else {
+                    # Send NACK!
+                }
+
+                $$buffref = "";
 
                 return 0;
             }
