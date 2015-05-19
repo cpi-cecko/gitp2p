50736416fce3d7e53d53920b4a8dc79400edc408
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-05-19 13:25:56 +0300

    relay caches peers in a file

--- gitp2p-relay.pl
+++ gitp2p-relay.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use v5.20;
 
+use Path::Tiny;
 use Method::Signatures;
 use IO::Async::Stream;
 use IO::Async::Loop;
@@ -22,9 +23,19 @@ $loop->listen(
             on_read => sub {
                 my ($self, $buffref, $eof) = @_;
 
-                while ($$buffref =~ s/^(.*?:.*?)//) {
-                    print "Received a line $1\n";
-                    $self->write("ACK!");
+                while ($$buffref =~ s/^(.*:.*)//) {
+                    my $addr = $self->read_handle->peerhost;
+                    my $port = $self->read_handle->peerport;
+                    my $peer_entry = $1 . ' ^ ' . $addr . ':' . $port . "\n";
+                    print "Received entry: $peer_entry";
+
+                    my $peers = path("peers");
+                    if ($peers->exists && grep { /\Q$peer_entry\E/ } $peers->lines) {
+                        $self->write("NACK: already added\n");
+                    } else {
+                        $self->write("ACK!\n");
+                        $peers->append(($peer_entry));
+                    }
                 }
 
                 return 0;
@@ -38,5 +49,6 @@ $loop->listen(
     on_resolve_error => sub { print STDERR "Cannot resolve - $_[0]\n"; },
     on_listen_error => sub { print STDERR "Cannot listen\n"; },
 );
+
 
 $loop->run;
