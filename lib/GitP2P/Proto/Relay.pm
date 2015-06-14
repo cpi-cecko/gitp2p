145461de93ada6f328cd99d29a88d30f15fa6093
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-06-14 12:21:41 +0300

    adds versioning to the Relay protocol

--- lib/GitP2P/Proto/Relay.pm
+++ lib/GitP2P/Proto/Relay.pm
@@ -1,30 +1,50 @@
+# 
+# The Relay protocol is used by the client to communicate with the relays. It
+# may become obsolete at the moment when we start using the peer-to-peer 
+# network alone for keeping the peer data.
+#
+# Relay protocol format ABNF
+# ==========================
+#
+# message = header SP op-name SP op-data
+#
+# header = version
+# op-name = *("_" "a"."z" "A"."Z")
+# op-data = *(ALNUM ":")
+#
+# version = major "." minor "." patch ["." meta]
+# major = *DIGIT
+# minor = *DIGIT
+# pathc = *DIGIT
+# mata = *ALNUM . "-" . "."
+#
 package GitP2P::Proto::Relay;
 
-use v5.20;
+use v5.020;
 
 use Moose;
 use Method::Signatures;
 
 
+my $VERSION = "0.1.0";
+
 has 'op_name' => ('is' => 'rw', 'isa' => 'Str');
 has 'op_data' => ('is' => 'rw', 'isa' => 'Str');
+has 'version' => ('is' => 'rw', 'isa' => 'Str');
 
-
-# relay protocol format ABNF
-#
-# message = op-name SP op-data
-#
-# op-name = *("_" "a"."z" "A"."Z")
-# op-data = *(ALNUM ":")
 
 method parse(Str $data) {
-    my ($op_name, $op_data) = split / /, $data;
+    my ($version, $op_name, $op_data) = split / /, $data;
+
+    die ("Incompatible version") if $version ne $VERSION;
+
+    $self->version($version);
     $self->op_name($op_name);
     $self->op_data($op_data);
 }
 
 func build(Str $op_name is ro, ArrayRef[Str] $op_data is ro) {
-    $op_name . " " . join ":", @$op_data;
+    $VERSION . " " . $op_name . " " . join ":", @$op_data;
 }
 
 
