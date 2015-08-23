c5c8147700a1351b6df2d5773a4073001e862af2
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-08-23 11:55:22 +0300

    Properly cleans up test dir.

--- t/clean-up.sh
+++ t/clean-up.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-rm -r test-users
-rm *.log
-rm peers.sqlite
+if [ -d test-users ]; then
+    rm -r test-users
+fi
+
+if [ -d log ]; then
+    [ "$(ls -A log 2> /dev/null)" != "" ] &&\
+        rm log/*
+fi
+
+if [ -f *.log ]; then
+    rm *.log
+fi
+
+if [ -f peers.sqlite ]; then
+    rm peers.sqlite
+fi
